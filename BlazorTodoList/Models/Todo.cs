d36d8dd42a88b693ddc2afff524bf6da11bd68f3
kapilaku <37741394+kapilaku@users.noreply.github.com>
2024-02-08 23:14:28 +0800

    Add project files.

---
+++ BlazorTodoList/Models/Todo.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorTodoList.Models;
+
+public class Todo
+{
+    public string Title { get; set; }
+    public bool IsDone { get; set; }
+}
---
+++ BlazorTodoList/Program.cs
@@ -0,0 +1,23 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace BlazorTodoList;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddFluentUIComponents();
+        builder.Services.AddBlazoredLocalStorage();
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        await builder.Build().RunAsync();
+    }
+}
